49b229a1ff3d3a1afa76a00a810111a4f33188ce
Praseeda <praseeeda@gmail.com>
2025-12-19 20:31:41 +0530

    update provider route corrected

--- src/pages/ProviderPages/ViewProviderProfile.jsx
+++ src/pages/ProviderPages/ViewProviderProfile.jsx
@@ -134,7 +134,7 @@ function ViewProviderProfile() {
                   borderRadius: "12px",
                   backgroundColor: "#4e73df",
                 }}
-                onClick={() => (window.location.href = "viewprovider/updateprovider")}
+                onClick={() => (window.location.href = "/providerDashboard/viewprovider/updateprovider")}
               >
                 Update Profile
               </Button>
