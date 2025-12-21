2ea198482718a01081bfbe956b4889507f09b968
Praseeda <praseeeda@gmail.com>
2025-12-21 23:02:38 +0530

    bootstrap import rearranged

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import App from './App.jsx'
+
 import './index.css'
 
 
