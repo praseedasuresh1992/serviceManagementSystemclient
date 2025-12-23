8a33b2b84516f09d428413ddba94c9f22d4c6904
Praseeda <praseeeda@gmail.com>
2025-12-23 22:27:55 +0530

    in view all request  api importcorrected

--- src/pages/ProviderPages/ViewAllRequest.jsx
+++ src/pages/ProviderPages/ViewAllRequest.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../config/axiosinstance";
+import api from "../../config/axiosinstance"
 import { format, differenceInDays } from "date-fns";
 
 const ViewAllRequest = () => {
